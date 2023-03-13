b88f50834ca01ed803f10ab546a4e2a0db661f0a
Zack123610 <zack123610@gmail.com>
2023-03-13 08:12:04 +0000

    new layout

--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 
 // import { MEALS } from "../data/dummy-data";
 // import { MallItem } from "../components/MallItem";
 
+import app from "../data/dummy-data.json"
+
+
+const StoreItem = ({ storeName, imageUrl }) => (
+    <View style={styles.storeItem}>
+        <View style={styles.innerContainer}>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{storeName}</Text>
+        </View>
+    </View>
+);
+
+function renderStoreItem({ item }) {
+    return(
+        <StoreItem storeName={item.storeName} imageUrl={item.imageUrl}/>
+    );
+}
+
 function ResultScreen(){
+    const DATA = [ app.Mcdonalds["Vivo City"], app.Watson["Vivo City"], app["NTUC FairPrice"]["Vivo City"] ]
     return (
-        <Text>Result Mall</Text>
+        <FlatList
+            data={DATA} 
+            renderItem={renderStoreItem}
+            keyExtractor={(item) => item.id}
+        />
     );
 }
 
@@ -34,4 +57,33 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 16,
     },
+    storeItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    image: {
+        width: '100%',
+        height: 100,
+    },
 })
+

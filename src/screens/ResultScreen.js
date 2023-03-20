b1d5a14bfa8a52aef897b8018dbd5a29e3e8162e
Zack123610 <zack123610@gmail.com>
2023-03-20 10:19:11 +0000

    updated with data passing

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,41 +1,79 @@
 import { View, Text, StyleSheet, FlatList, Image, Button } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
-import app from "../data/dummy-data.json";
+// import app from "../data/dummy-data.json";
+import newdata from "../data/database.json";
 
-const StoreItem = ({ storeName, imageUrl }) => (
+const StoreItem = ({ storeName, imageUrl, unitNumber, contact }) => (
     <View style={styles.storeItem}>
         <View style={styles.innerContainer}>
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{storeName}</Text>
+            <Text style={styles.title}>{unitNumber}</Text>
+            <Text style={styles.title}>{contact}</Text>
         </View>
     </View>
 );
 
 function renderStoreItem({ item }) {
     return(
-        <StoreItem storeName={item.storeName} imageUrl={item.imageUrl}/>
+        <StoreItem 
+            storeName={item.storeName} 
+            imageUrl={item.imageStore}
+            unitNumber={item.unitNumber}
+            contact={item.contact_number}
+        />
     );
 }
 
-function ResultScreen(){
+function findMallIndex(resultMall){
+    var i;
+    for (i=0; i<newdata.Malls.length; i++){
+        if (newdata.Malls[i].mallId === resultMall) break;
+    };
+    return i;
+}
+
+function findStoreIndex(resultStore, storesInMall){
+    var i;
+    for (i=0; i<storesInMall.length; i++){
+        if (storesInMall[i].storeName === resultStore) break;
+    };
+    return i;
+}
+
+function ResultScreen( {route} ){
     const navigation = useNavigation();
+    const resultMall = route.params.resultMall;
+    const resultStores = route.params.resultStores;
 
     function buttonPressHandler() {
         navigation.navigate('CarparkOrMallScreen');
     }
 
-    const DATA = [ app.Mcdonalds["Vivo City"], app.Watson["Vivo City"], app["NTUC FairPrice"]["Vivo City"] ]
+    const mallIndex = findMallIndex(resultMall);
+    const storesInMall = newdata.Malls[mallIndex].stores;
+
+    var dummy = [];
+    var storeIndex;
+    for (let x=0; x<resultStores.length; x++){
+        storeIndex = findStoreIndex(resultStores[x], storesInMall);
+        dummy.push(storesInMall[storeIndex]);
+    }
+
+    const DATA = dummy;
     return (
         <>
         <FlatList
             data={DATA}
             renderItem={renderStoreItem}
-            keyExtractor={(item) => item.id} />
+            // keyExtractor={(item) => item.id} 
+        />
         <Button style={styles.button} title='DONE' onPress={buttonPressHandler} />
         </>
     );
 }
+
 
 export default ResultScreen;
 
@@ -47,7 +85,7 @@ const styles = StyleSheet.create({
     storeItem: {
         flex: 1,
         margin: 16,
-        height: 150,
+        height: 200,
         borderRadius: 8,
         elevation: 4,
         backgroundColor: 'white',

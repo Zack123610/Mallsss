9064417621882cfa7cd3208165a6e8dfcf7166cb
Zack123610 <zack123610@gmail.com>
2023-03-26 16:57:59 +0000

    2 apps do not work, so only 1

--- config.js
+++ config.js
@@ -5,10 +5,5 @@ const app1 = initializeApp({
     databaseURL: "https://mallsss-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
-const app2 = initializeApp({
-    databaseURL: "https://mallsss-history.asia-southeast1.firebasedatabase.app"
-});
-
 const db = getDatabase(app1);
-const db_history = getDatabase(app2)
-export { db, db_history };
+export { db };

5f9f4d82b5309966fe329d3696455b4957264ebd
Zack123610 <zack123610@gmail.com>
2023-03-29 05:32:27 +0000

    updated with carpark option

--- src/screens/CarparkOrMallScreen.js
+++ src/screens/CarparkOrMallScreen.js
@@ -3,29 +3,36 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function CarparkOrMallScreen( {route} ) {
-    const navigation = useNavigation();
-    const resultMall = route.params.resultMall;
-    const resultStores = route.params.resultStores;
+  const navigation = useNavigation();
+  const resultMall = route.params.resultMall;
+  const resultStores = route.params.resultStores;
 
-    function mapSelectedHandler() {
+  function mallChoiceHandler() {
+    console.log("Mall Clicked.");
       navigation.navigate('Map', {
+        choice: "mall",
         resultMall: resultMall,
         resultStores: resultStores,
       });
-    }
+  }
 
-    function carparkChoiceHandler() {
-        console.log("Carpark Clicked.");
-    }
+  function carparkChoiceHandler() {
+      console.log("Carpark Clicked.");
+      navigation.navigate('Map', {
+        choice: "carpark",
+        resultMall: resultMall,
+        resultStores: resultStores,
+      });
+  }
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.carparkOrMallContainer}>
-                <View style={styles.optionContainer}><Button title="Carpark" onPress={carparkChoiceHandler} /></View>
-                <View style={styles.optionContainer}><Button title="Mall" onPress={mapSelectedHandler} /></View>
-            </View>
-        </View>
-    );
+  return (
+      <View style={styles.container}>
+          <View style={styles.carparkOrMallContainer}>
+              <View style={styles.optionContainer}><Button title="Carpark" onPress={carparkChoiceHandler} /></View>
+              <View style={styles.optionContainer}><Button title="Mall" onPress={mallChoiceHandler} /></View>
+          </View>
+      </View>
+  );
 }
 
 export default CarparkOrMallScreen;
